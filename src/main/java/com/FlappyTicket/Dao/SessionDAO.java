e3351e91bfa5e8719d2f354699e3dc220285a353
czxyml <1933443205@qq.com>
2016-06-04 21:07:29 +0800

    新增三个session find方法

--- src/main/java/com/FlappyTicket/Dao/SessionDAO.java
+++ src/main/java/com/FlappyTicket/Dao/SessionDAO.java
@@ -20,23 +20,66 @@ public class SessionDAO {
 	private JdbcTemplate jdbcTemplate;
 	
 	public List<Session> findAll() {
-		return jdbcTemplate.query("select * from Session", new BeanPropertyRowMapper<Session>(Session.class));
+		return jdbcTemplate.query("select * from session", new BeanPropertyRowMapper<Session>(Session.class));
 	}
 	
-	public Session findById(int mid) {
+	public Session findById(int sid) {
 		try {
-			return jdbcTemplate.queryForObject("select * from Session where SID = ?", new BeanPropertyRowMapper<Session>(Session.class), mid);
+			return jdbcTemplate.queryForObject("select * from session where SID = ?", new BeanPropertyRowMapper<Session>(Session.class), sid);
 		} catch (EmptyResultDataAccessException e) {
 			return null;
 		}
 	}
 	
-	public Session findBySessionName(String mname) {
+	public List<Session> findByCID(int cid) {
 		try {
-			return jdbcTemplate.queryForObject("select * from Session where SName = ?", new BeanPropertyRowMapper<Session>(Session.class), mname);
+			return jdbcTemplate.query("select * from session where CID = ?", new BeanPropertyRowMapper<Session>(Session.class), cid);
 		} catch (EmptyResultDataAccessException e) {
 			return null;
 		}
 	}
+	
+	public List<Session> findByMID(int mid) {
+		try {
+			return jdbcTemplate.query("select * from session where MID = ?", new BeanPropertyRowMapper<Session>(Session.class), mid);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+	
+	public List<Session> findByCMID(int cid, int mid) {
+		try {
+			return jdbcTemplate.query("select * from session where CID = ? and MID = ?", new BeanPropertyRowMapper<Session>(Session.class), cid, mid);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+	
+	/*public int insert(final Session Session) {
+		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(
+				new PreparedStatementCreator() {
+					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+						PreparedStatement st = conn.prepareStatement("insert into Session(MName, MType, MDetail, MTime, MPicture) values (?, ?, ?, ?, ?)", new String[]{"id"});
+						st.setString(1, Session.getmName());
+						st.setString(2, Session.getmType());
+						st.setString(3, Session.getmDetail());
+						st.setString(4, Session.getmTime());
+						st.setString(5, Session.getmPicture());
+						return st;
+					}
+				}, generatedKeyHolder
+		);
+		return generatedKeyHolder.getKey().intValue();
+	}
+	
+	public int update(final Session Session) {
+		return jdbcTemplate.update("update Session set MName = ?, MType = ?, MDetail = ?, MTime = ?, MPicture = ? where MID = ?", 
+				new Object[] {Session.getmName(), Session.getmType(), Session.getmDetail(), Session.getmTime(), Session.getmPicture(), Session.getMid()});
+	}
+	
+	public int delete(int mid) {
+		return jdbcTemplate.update("delete from Session where MID =" + mid + "");
+	}*/
 	
 }
