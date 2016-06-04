49e5d77f7e872a0af2df3e08a4d6f3c420eb5380
czxyml <1933443205@qq.com>
2016-06-04 20:45:30 +0800

    change

--- src/main/java/com/FlappyTicket/Dao/SessionDAO.java
+++ src/main/java/com/FlappyTicket/Dao/SessionDAO.java
@@ -39,31 +39,6 @@ public class SessionDAO {
 		}
 	}
 	
-	/*public int insert(final Session Session) {
-		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
-		jdbcTemplate.update(
-				new PreparedStatementCreator() {
-					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-						PreparedStatement st = conn.prepareStatement("insert into Session(MName, MType, MDetail, MTime, MPicture) values (?, ?, ?, ?, ?)", new String[]{"id"});
-						st.setString(1, Session.getmName());
-						st.setString(2, Session.getmType());
-						st.setString(3, Session.getmDetail());
-						st.setString(4, Session.getmTime());
-						st.setString(5, Session.getmPicture());
-						return st;
-					}
-				}, generatedKeyHolder
-		);
-		return generatedKeyHolder.getKey().intValue();
-	}
 	
-	public int update(final Session Session) {
-		return jdbcTemplate.update("update Session set MName = ?, MType = ?, MDetail = ?, MTime = ?, MPicture = ? where MID = ?", 
-				new Object[] {Session.getmName(), Session.getmType(), Session.getmDetail(), Session.getmTime(), Session.getmPicture(), Session.getMid()});
-	}
-	
-	public int delete(int mid) {
-		return jdbcTemplate.update("delete from Session where MID =" + mid + "");
-	}*/
 	
 }
